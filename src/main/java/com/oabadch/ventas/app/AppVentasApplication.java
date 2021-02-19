8ae123cb582b34b8b1a305d03e4456547676e033
oli79 <oabadch@gmail.com>
2021-02-19 20:14:38 +0100

    ini proyect app-ventas

---
+++ src/main/java/com/oabadch/ventas/app/AppVentasApplication.java
@@ -0,0 +1,13 @@
+package com.oabadch.ventas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oabadch/ventas/app/AppVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oabadch.ventas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
